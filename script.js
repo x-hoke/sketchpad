3092c4ffb8e068eb665266dcb5eb442579c98e70
x-hoke <danylosimchuk@gmail.com>
2023-05-11 12:26:58 +0300

    added extandable grid

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 let container = document.getElementById("sketchpad");
-        
-let gridSize = 16;
-let squareSize = 22;
+let magicButton = document.getElementById("magic-button");        
 
-container.style.width = gridSize * squareSize + "px";
+function createGrid(gridSize){
+    container.innerHTML = "";
+    const containerWidth = container.offsetWidth;
+    const squareSize = containerWidth / gridSize - 2;
 
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            container.appendChild(square);
 
-
-for (let i = 0; i < 16; i++) {
-            for (let j = 0; j < 16; j++) {
-                const square = document.createElement("div");
-                square.classList.add("square");
-                container.appendChild(square);
-            }
-            const lineBreak = document.createElement("br");
-            container.appendChild(lineBreak);
+            square.addEventListener("mouseover",() =>{
+                square.style.backgroundColor = "black"
+            })
+            setTimeout(() => {
+                square.style.backgroundColor = ""
+            }, 500)
         }
+        const lineBreak = document.createElement("br");
+        container.appendChild(lineBreak);
+    }
+
+}
+
+magicButton.addEventListener("click", () =>{
+    let input = prompt("Enter the number of squares for per side (max 100)");
+    let gridSize = parseInt(input);
+
+    if(isNaN(gridSize) || gridSize < 1 || gridSize > 100){
+        alert("invalid input. Please enter proper number between 1 and 100")
+    }else{
+        createGrid(gridSize)
+    }
+})
+// inital grid
+createGrid(16)
+
+
+
+
+
+
